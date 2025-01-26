53e72705401b14bbebaf9782cf7927be080322cb
Manpreet Singh <singhman2005123@gmail.com>
2025-01-27 01:44:52 +0530

    pipeline for transform

---
+++ src/pipeline/data_transform_pipeline.py
@@ -0,0 +1,11 @@
+from src.config.configuration import configmanager
+from src.components.datatransform import Datatransform
+
+class DataIngestionPipeline:
+    def __init__(self):
+        self.config_manager = configmanager()
+
+    def run(self):
+        config = self.config_manager.get_data_transform_config()
+        data_ingestion = Datatransform(config)
+        data_ingestion.run()
