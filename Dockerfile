8dc18705c1e59afb883b57ba331d0b438d535be2
Manpreet Singh <singhman2005123@gmail.com>
2025-01-28 18:17:37 +0530

    added actions to build & push

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM quay.io/astronomer/astro-runtime:12.6.0
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirement.txt
+CMD astro dev start
--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
-from src.pipeline.data_transform_pipeline import DataTransformPipeline
-from src.pipeline.model_train_pipeline import trainingpiepline
-from src.pipeline.model_test_pipeline import testingpipeline
-if __name__ == "__main__":
-    print('started ingestion')
-    pipeline1 = DataIngestionPipeline()
-    pipeline1.run()
-    print('end of ingestion')
-
-    print('started transform')
-    pipeline2 = DataTransformPipeline()
-    pipeline2.run()
-    print('end of transform')
+#moved this code to dags folder
 
 
-    print('started training')
-    pipeline3 = trainingpiepline()
-    pipeline3.run()
-    print('end of traning')
+# from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+# from src.pipeline.data_transform_pipeline import DataTransformPipeline
+# from src.pipeline.model_train_pipeline import trainingpiepline
+# from src.pipeline.model_test_pipeline import testingpipeline
+# if __name__ == "__main__":
+#     print('started ingestion')
+#     pipeline1 = DataIngestionPipeline()
+#     pipeline1.run()
+#     print('end of ingestion')
 
-    print('started testing')
-    pipeline4 = testingpipeline()
-    pipeline4.run()
-    print('end of testing')
+#     print('started transform')
+#     pipeline2 = DataTransformPipeline()
+#     pipeline2.run()
+#     print('end of transform')
+
+
+#     print('started training')
+#     pipeline3 = trainingpiepline()
+#     pipeline3.run()
+#     print('end of traning')
+
+#     print('started testing')
+#     pipeline4 = testingpipeline()
+#     pipeline4.run()
+#     print('end of testing')
